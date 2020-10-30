b229c485808035c9c7467338bd7498191ef3a40c
Erik Nelson <erik.david.nelson@gmail.com>
2020-10-30 14:31:27 -0400

    Add ubuntu bootstrap script

---
+++ bootstrap.ubuntu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Update APT sources
+sudo apt -y update 
+sudo apt -y upgrade
+
+# APT packages
+sudo apt -y install \
+    build-essential \
+    libssl-dev \
+    pkg-config \
+    zip \
+    unzip \
+    micro \
+    curl \
+    fzf \
+    zsh
+
+# Micro editor
+curl https://getmic.ro | bash
+
+# Rust & Cargo
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Crates
+cargo install exa sheldon
+
+# SDKMan
+curl -s "https://get.sdkman.io" | bash
